cf5f986a56013fc67ab2fc38a3f5f658ac8794ed
Cloud User <ec2-user@ip-172-31-23-183.ec2.internal>
2022-05-24 20:05:06 +0000

    first version

---
+++ hello.sh
@@ -0,0 +1,6 @@
+
+echo "Welcome to Winners Chapel Toronto"
+echo "I am a millionaire"
+echo "Today is the:"
+date
+echo "Thank you for coming"
---
+++ winners.sh
@@ -0,0 +1,5 @@
+echo choir
+echo media
+echo pastor
+echo hospitality
+echo keeper
